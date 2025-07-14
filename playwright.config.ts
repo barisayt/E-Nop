3692021ac84bb020db55b74829007245952eab4a
barisayt <netshell0@gmail.com>
2025-07-14 05:34:28 -0400

    Update

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,11 +23,10 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: undefined,
-  timeout: 20000,
+  timeout: 10000,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html', { outputFolder: 'playwright-report', open: 'never' }], //Playwright HTML Report
-    ['allure-playwright'], //Allure report file
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
